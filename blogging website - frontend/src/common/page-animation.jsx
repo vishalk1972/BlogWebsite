02ee8eb829daf2591e9d5a88e586139cd14cc3ef
vishalk1972 <vishalkuwar000@gmail.com>
2024-01-19 14:18:58 +0530

    animation done

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,12 +1,14 @@
 import {AnimatePresence,motion} from "framer-motion"
 const AnimationWrapper=({children,keyId,initial={opacity:0},animate={opacity:1},transition={duration:1}})=>{
-    return <motion.div
-        key={keyId}
-        initial={initial}
-        animate={animate}
-        transition={transition}
-    >
-        {children}
-    </motion.div>
+    return <AnimatePresence>
+        <motion.div
+            key={keyId}
+            initial={initial}
+            animate={animate}
+            transition={transition}
+        >
+            {children}
+        </motion.div>
+    </AnimatePresence>
 }
 export default AnimationWrapper;
